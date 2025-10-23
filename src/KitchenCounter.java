51cd94cfeabfbb9b6931da4605498ba95e719ba3
Noah Jutz <noah@noahjutz.com>
2025-10-23 13:47:58 +0200

    Implement thread-safe put and take

--- src/KitchenCounter.java
+++ src/KitchenCounter.java
@@ -1,7 +1,42 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class KitchenCounter {
     private final int capacity;
+    private int load;
+
+    private final Lock loadLock = new ReentrantLock();
+    private final Condition putCondition = loadLock.newCondition();
+    private final Condition takeCondition = loadLock.newCondition();
 
     public KitchenCounter(int capacity) {
         this.capacity = capacity;
+    }
+
+    public void put() {
+        loadLock.lock();
+        while (load >= capacity) {
+            try {
+                putCondition.await();
+            } catch (InterruptedException e) {
+            }
+        }
+        load++;
+        takeCondition.notify();
+        loadLock.unlock();
+    }
+
+    public void take() {
+        loadLock.lock();
+        while (load <= 0) {
+            try {
+                takeCondition.await();
+            } catch (InterruptedException e) {
+            }
+        }
+        load--;
+        putCondition.notify();
+        loadLock.unlock();
     }
 }
